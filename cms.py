75ce97957a01f66ba63edc8159caa9d23a7660fb
Yevgen ZK <zilinchuk@gmail.com>
2016-03-22 21:51:50 -0700

    updates to check_service

--- cms.py
+++ cms.py
@@ -3,10 +3,16 @@
 import subprocess
 import os
 import time
+import filecmp
 
-package_name = 'apache'
+"""
+#Debugging variables:
+package_name = 'apache2'
 service_name = 'apache2'
 config_name = '/var/www/html/index.php'
+config_source = 'index.php'
+"""
+
 DEVNULL = open(os.devnull, 'w')
 
 
@@ -20,6 +26,15 @@ def check_package(package_name):
 		time.sleep(1)
     		subprocess.call("apt-get install -y '%s'" % package_name, shell = True)
 
+def check_config(config_name,config_source):
+	"""Verifies if config file exists"""
+	if os.path.exists(config_name) == True and filecmp.cmp(config_source, config_name):
+		print "%s is present and content matches source" % config_name
+	else:
+		print "%s is missing or content is different from source. Re-installing..." % config_name
+		time.sleep(1)
+		subprocess.call("cp '%s' '%s'" % (config_source, config_name), shell = True)
+
 def check_service(service_name):
 	"""Restarts service if it is not running"""
 	check = subprocess.call("service '%s' status" % service_name, stdout = DEVNULL, shell = True)
@@ -30,14 +45,9 @@ def check_service(service_name):
 		time.sleep(1)
 		subprocess.call("service '%s' restart" % service_name, shell = True)
 
-def check_config(config_name):
-	"""Verifies if config file exists"""
-	if os.path.exists(config_name) == True:
-		print "Config file is present"
-	else:
-		print "Config file is missing."
-
-
+"""
+#Test run
 check_package(package_name)
-check_service(service_name)
 check_config(config_name)
+check_service(service_name)
+"""
