5af1c2eb8ef3c3560b20a65ef7c5c6baebeb0b47
Yevgen ZK <zilinchuk@gmail.com>
2016-03-24 16:11:11 -0700

    adding configuration file

--- webconfig.py
+++ webconfig.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python
 
+import ConfigParser
 from cms import *
 
-"""Debug configuration"""
-packages = ['php5', 'apache2']
-services = ['apache2']
-config_pairs = {
-	'/var/www/html/index.php' : 'index.php',
-	'/var/www/html/dummy.php' : 'dummy.php'
-}
+parser = ConfigParser.ConfigParser()
+parser.read('config.cfg')
 
-if len(packages) != 0:
-	for pkg in packages:
-		check_package(pkg)
+"""Iterates over every section in the config file
+and verifies if item exist. If it does it takes takes action against it."""
 
-if len(config_pairs) != 0:
-	for cfg in config_pairs:
-		check_config(cfg,config_pairs[cfg])
+for section in parser.sections():
+        if parser.has_option(section, 'package_name'):
+                check_package(parser.get(section,'package_name'))
 
-if len(services) != 0:
-	for srvc in services:
-		check_service(srvc)
+        if parser.has_option(section, 'service_name'):
+                check_service(parser.get(section,'service_name'))
+
+        if parser.has_option(section, 'config_source') and parser.has_option(section, 'config_path'):
+                check_config(parser.get(section,'config_path'),parser.get(section,'config_source'))
