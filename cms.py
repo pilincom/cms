0853a940bd8ab785537707cc89cd6f4fc3961c0e
Yevgen ZK <zilinchuk@gmail.com>
2016-03-25 19:14:09 -0700

    beta version

--- cms.py
+++ cms.py
@@ -20,7 +20,7 @@ def check_package(package_name):
     		subprocess.call("apt-get install -y '%s'" % package_name, shell = True)
 
 def check_config(config_name,config_source):
-	"""Verifies if config file exists"""
+	"""Verifies if config file exists and content matches source"""
 	if os.path.exists(config_source) != True:
 		print "%s is missing at the source location" % config_source
 		return
@@ -38,7 +38,7 @@ def check_service(service_name):
 	if check == 0:
 		print "Service %s is running" % service_name
 	else:
-		print "Service %s is not running. Attempting to restart..." % service_name
+		print "Service %s is not running. Restarting..." % service_name
 		time.sleep(1)
 		subprocess.call("service '%s' restart" % service_name, shell = True)
 
@@ -47,7 +47,7 @@ def config_stat(filename,owner,group,mode):
         if pwd.getpwuid(os.stat(filename).st_uid).pw_name == owner or pwd.getpwuid(os.stat(filename).st_gid).pw_name == group:
                 print "Ownership of %s is correct" % filename
         else:
-                print "Ownershipof %s is incorrect. Fixing..." % filename
+                print "Ownership of %s is incorrect. Fixing..." % filename
                 time.sleep(1)
                 os.chown(filename, pwd.getpwnam(owner).pw_uid, pwd.getpwnam(group).pw_gid)
 
@@ -55,6 +55,6 @@ def config_stat(filename,owner,group,mode):
         if oct(os.stat(filename).st_mode)[-4:] == mode:
                 print "Permissions of %s are correct" % filename
         else:
-                print "Permissions of %s are incorrect. Fixing" % filename
+                print "Permissions of %s are incorrect. Fixing..." % filename
                 time.sleep(1)
                 os.chmod(filename,int(mode,8))
--- webconfig.py
+++ webconfig.py
@@ -7,7 +7,7 @@ parser = ConfigParser.ConfigParser()
 parser.read('config/config.cfg')
 
 """Iterates over every section in the config file
-and verifies if item exist. If it does it takes takes action against it."""
+and verifies if item exist. If it does it takes takes action accordingly."""
 
 for section in parser.sections():
         print '-' * len(section)
@@ -18,7 +18,8 @@ for section in parser.sections():
                 check_package(parser.get(section,'package_name'))
 
         if parser.has_option(section, 'config_path'):
-                if parser.has_option(section, 'config_source') != True or parser.has_option(section, 'config_owner') != True or parser.has_option(section, 'config_group') != True or parser.has_option(section, 'config_perm') != True:
+                if parser.has_option(section, 'config_source') != True or parser.has_option(section, 'config_owner') != True or parser.has_option(section, 'config_group') != True \
+                 or parser.has_option(section, 'config_perm') != True:
                         print "Source, owner, group and/or permissions is missing from the config file. Please correct it and run again."
                 else:
                         check_config(parser.get(section,'config_path'),parser.get(section,'config_source'))
